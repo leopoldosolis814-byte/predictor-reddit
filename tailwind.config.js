940d474907d0c447e10dc4d5d1794e371392b7cb
leopoldosolis814-byte <leopoldosolis814@gmail.com>
2026-04-23 05:01:28 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
-hkkjh
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
